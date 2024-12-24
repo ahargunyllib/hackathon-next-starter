9ed96e2fdb184f7b560170346b7c813a849eac69
ahargunyllib <billy.bpm03@gmail.com>
2024-12-24 20:21:29 +0700

    fix: css import & theme provider

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,14 +1,11 @@
-import { ThemeProvider } from "next-themes";
+"use client";
+
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import type React from "react";
 
 export default function Provider({ children }: React.PropsWithChildren) {
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			enableSystem
-			disableTransitionOnChange
-		>
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 			{children}
 		</ThemeProvider>
 	);
