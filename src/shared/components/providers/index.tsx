adc6e59a75b48f8a1d2125682dc0a204f7d4dffe
romves <nofalrmz@gmail.com>
2025-01-10 23:54:27 +0700

    feat: add toaster and form components

--- src/shared/components/providers/index.tsx
+++ src/shared/components/providers/index.tsx
@@ -2,11 +2,13 @@
 
 import { ThemeProvider } from "@/shared/components/providers/ThemeProvider";
 import type React from "react";
+import { Toaster } from "../ui/sonner";
 
 export default function Provider({ children }: React.PropsWithChildren) {
 	return (
 		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 			{children}
+			<Toaster />
 		</ThemeProvider>
 	);
 }
